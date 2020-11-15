be7daddd191179e117a32cd4fb2578239ae18529
Oliver Stocks <stocksolly@gmail.com>
2020-11-15 18:03:47 +0000

    initial commit

---
+++ basicio.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int var1 = 13;
+    int var2 = 15;
+    int sol;
+
+    cout << "\t" << var1 << "\n";
+    cout << "+\t" << var2 << "\n" << "-----";
+
+    cout << "What is the solution? "; 
+    cin >> var2;
+
+    return 0;
+}
